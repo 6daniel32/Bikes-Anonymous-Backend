d25a6e71c81e554ac9f4e79a6d80906630c634fc
Daniel <6daniel32@gmail.com>
2021-02-21 21:51:22 +0000

    PDF license creation

--- src/api/services/licenseCreationService.ts
+++ src/api/services/licenseCreationService.ts
@@ -1,7 +1,26 @@
 import BikeRiderInfo from '../types/bikeRiderInfo';
-import { PDFDocument } from 'pdf-lib';
+import { PDFDocument, StandardFonts,rgb } from 'pdf-lib';
 
-export default function(bikeRider: BikeRiderInfo) {
-    console.log(bikeRider);
+const fs = require('fs');
+
+export default async function(bikeRider: BikeRiderInfo) {
+    const pdfDoc = await PDFDocument.create();
+    const page = pdfDoc.addPage();
+    const { width, height } = page.getSize()
+    const timesRomanFont = await pdfDoc.embedFont(StandardFonts.TimesRoman);
+    const fontSize = 15;
+    page.drawText(
+        "License holder: " + bikeRider.firstName + " " + bikeRider.lastName + "\n" +
+        "ID: " + bikeRider.ID + "\n" +
+        "email: " + bikeRider.email + "\n" +
+        "phone: " + bikeRider.phone,
+    {
+        x: 50,
+        y: height - 4 * fontSize,
+        size: fontSize,
+        font: timesRomanFont,
+    });
+    const pdfBytes = await pdfDoc.save();
+    fs.createWriteStream('./uploads/myFile.pdf').write(pdfBytes);
     return;
 }
